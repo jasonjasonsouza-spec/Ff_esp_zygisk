8d8cb8e524054d171bdd9eee67aaa3bc4b362618
jasonjasonsouza-spec <jasonjasonsouza@gmail.com>
2026-08-27 17:11:39 -0300

    Create offset.h with game entity offsets
    Added offsets for various game entities and player states.

---
+++ app/src/main/cpp/offset.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#define INIT_BASE                0xAAA5B20
+#define CURRENT_MATCH            0x50
+#define LOCAL_PLAYER             0x94
+#define ENTITY_DICTIONARY        0x68
+#define PLAYER_IS_DEAD           0x50
+#define PLAYER_IS_TEAMMATE       0x59
+#define AVATAR_INSTANCE          0xA8
+#define AVATAR_IS_VISIBLE        0x95
+#define CAMERA_TRANSFORM         0x250
+#define MATRIX_WORLD_TO_SCREEN   0xE8
+#define BONE_HEAD                0x460
+#define BONE_HIP                 0x464
+#define BONE_RIGHT_FOOT          0x488
+#define DICT_PAIRS_OFFSET        0x40
+#define DICT_COUNT_OFFSET        0x48
+#define DICT_ENTITY_OFFSET       0x08
+#define TRANSFORM_NODE_POS       0x28
